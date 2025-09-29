976d5aabf1f21a43fda55e7458bbc8419a5f8a5e
Syed Zulqarnain Haider <haidershahpak17@gmail.com>
2025-09-29 21:38:49 +0500

    Loading Screen Height resolved

--- src/components/SeminarDetailsModal.tsx
+++ src/components/SeminarDetailsModal.tsx
@@ -51,13 +51,13 @@ const SeminarDetailsModal = ({ setIsVisible, selectedSeminar, handleSingleInquir
         return (
             <>
                 <div className={`bg-gray-950/30 fixed top-0 left-0 w-full h-full z-20 ${selectedSeminar ? 'block' : 'hidden'}`} onClick={() => setIsVisible(false)} />
-                <div className="fixed top-1/2 left-1/2 -translate-1/2 w-full max-w-[700px] h-[80vh] max-h-[600px] overflow-y-auto bg-white rounded-[10px] p-5 pt-0 z-30">
+                <div className="fixed top-1/2 left-1/2 -translate-1/2 w-full max-w-[700px] max-h-[80vh]  overflow-y-auto bg-white rounded-[10px] p-5 pt-0 z-30">
                     <div className="sticky top-0 bg-white flex justify-between items-center border-b-[1px] border-[#ddd] pt-[20px] pb-[18px] mb-[12px]">
                         <h2 className="text-2xl font-bold">セミナー詳細</h2>
                         <span className="text-[38px] hover:text-red-500 hover:cursor-pointer" onClick={() => setIsVisible(false)}>&times;</span>
                     </div>
 
-                    <h3 className="py-[100px] text-center">詳細を読み込み中...</h3>
+                    <h3 className="py-[50px] text-center">詳細を読み込み中...</h3>
                 </div>
             </>
         )
